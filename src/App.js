9ce7a1da68f474eeaf7292d21a4855336796512b
pkaseta <kasetapaul@gmail.com>
2021-05-08 10:52:43 -0400

    added navabar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { Switch, Route } from 'react-router';
 import './App.css';
+import Home from "./views/home/Home"
+import Footer from "./components/footer/Footer"
+import Navbar from "./components/navbar/Navbar"
 
 function App() {
   return (
     <div>
-      <h1>Weather App</h1>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+      <Footer />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './components/navbar/Navbar.css'
+import './views/home/Home.css'
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
